3c8beb713a5b84208162038c3dab3452aa5a82be
BuleSky233 <365665061@qq.com>
2020-06-11 23:29:34 +0800

    新建了选课controller和选课service

---
+++ ClassRegistrationSystem/src/main/java/cn/edu/jlu/ccst/glzz/system/Controller/ChooseCoursesController.java
@@ -0,0 +1,21 @@
+package cn.edu.jlu.ccst.glzz.system.Controller;
+
+import cn.edu.jlu.ccst.glzz.system.Model.User;
+import cn.edu.jlu.ccst.glzz.system.Service.GradeService;
+import cn.edu.jlu.ccst.glzz.system.Util.JsonUtil;
+import cn.edu.jlu.ccst.glzz.system.Util.ReadFileUtil;
+import cn.edu.jlu.ccst.glzz.system.Util.Result;
+import cn.edu.jlu.ccst.glzz.system.generated.Model.Student;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class ChooseCoursesController {
+}
