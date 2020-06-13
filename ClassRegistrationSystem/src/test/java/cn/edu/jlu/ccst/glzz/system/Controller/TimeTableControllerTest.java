0bb7ca92cee8f3f743e4a213027ebcfb382c5d61
maxzheng <1836941753@qq.com>
2020-06-13 23:14:06 +0800

    课程表后端 + 换了新的数据库

---
+++ ClassRegistrationSystem/src/test/java/cn/edu/jlu/ccst/glzz/system/Controller/TimeTableControllerTest.java
@@ -0,0 +1,38 @@
+package cn.edu.jlu.ccst.glzz.system.Controller;
+
+import io.restassured.response.ValidatableResponse;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TimeTableControllerTest {
+
+    @Test
+    void getTimeTable() {
+        //成功登陆
+        ValidatableResponse myresponse =given()
+                .log().all()
+                .queryParam("userid","00128")
+                .queryParam("password","12345678")
+                .queryParam("usertype","student")
+                .when()
+                .post("/login")
+                .then()
+                .log().all()
+                .body("status",equalTo(200));
+        Map responseCookies = myresponse.extract().cookies();
+
+        given()
+                .log().all()
+                .cookies(responseCookies)
+                .when()
+                .get("/student/timetable.json")
+                .then()
+                .log().all()
+                .body("status",equalTo(200));
+    }
+}
