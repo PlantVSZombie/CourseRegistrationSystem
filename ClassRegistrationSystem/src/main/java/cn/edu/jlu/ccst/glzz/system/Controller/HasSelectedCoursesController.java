5a0023ce4b64b880948e88fd4213ad804c82e4dc
BuleSky233 <365665061@qq.com>
2020-06-14 00:52:55 +0800

    添加课程的基本功能完成

--- ClassRegistrationSystem/src/main/java/cn/edu/jlu/ccst/glzz/system/Controller/HasSelectedCoursesController.java
+++ ClassRegistrationSystem/src/main/java/cn/edu/jlu/ccst/glzz/system/Controller/HasSelectedCoursesController.java
@@ -21,21 +21,9 @@ public class HasSelectedCoursesController {
     @Resource
     HasSelectedCoursesService hasSelectedCoursesService;
 
-    List<Map<String, Object>> zhuList = new ArrayList<>();
-    List<Map<String, Object>> beiList = new ArrayList<>();
+
 
     public List<Map<String, Object>> getZhuList(int limit, int page, HttpSession session, String searchParams) throws IOException{
-        getSelectedMajorCourse(limit,page,session,searchParams);
-        return zhuList;
-    }
-
-    public List<Map<String, Object>> getBeiList(int limit, int page, HttpSession session, String searchParams) throws IOException {
-        getSelectedAlternativeCourse(limit,page,session,searchParams);
-        return beiList;
-    }
-
-    @RequestMapping(value = "/student/selected_major_courses.json", produces = "application/json;charset=UTF-8")
-    public JSONObject getSelectedMajorCourse(int limit, int page, HttpSession session, String searchParams) throws IOException {
         User user = (User) session.getAttribute("user");
         Student student = (Student) user.getPerson();
         JSONObject jsonObject;
@@ -44,98 +32,27 @@ public class HasSelectedCoursesController {
         } else {
             jsonObject = new JSONObject();
         }
-        List<Map<String, Object>> selectCoursesList = hasSelectedCoursesService.getHasSelectedCourses(student.getStudentId(), limit, page, jsonObject.getString("class_name"), jsonObject.getInteger("year"), jsonObject.getString("semester"),1);
+        return hasSelectedCoursesService.getHasSelectedCourses(student.getStudentId(), limit, page, jsonObject.getString("class_name"), jsonObject.getInteger("year"), jsonObject.getString("semester"),1);
 
-        //将相同class_id的合并
-        for (Map<String, Object> it : selectCoursesList) {
-            boolean newone = true;
-            //找在不在ansList里
-            for (Map<String, Object> ansit : zhuList) {
-                if (ansit.get("class_id") == it.get("class_id")) {
-                    newone = false;
-                    String tem = (String) ansit.get("course_place");
-                    tem += "<br>";
-                    tem += (String) it.get("building") + " " + (String) it.get("room_number");
-                    ansit.put("course_place", tem);
-                    String timetem = (String) ansit.get("course_time");
-                    timetem += "<br>";
-                    int i_day = (int) it.get("day");
-                    String s_day = null;
-                    switch (i_day) {
-                        case 1:
-                            s_day = "周一";
-                            break;
-                        case 2:
-                            s_day = "周二";
-                            break;
-                        case 3:
-                            s_day = "周三";
-                            break;
-                        case 4:
-                            s_day = "周四";
-                            break;
-                        case 5:
-                            s_day = "周五";
-                            break;
-                        case 6:
-                            s_day = "周六";
-                            break;
-                        case 7:
-                            s_day = "周日";
-                            break;
-                    }
 
-                    String start_time2 = (it.get("start_time").toString());
-                    start_time2 = start_time2.substring(0, 5);
-                    String end_time2 = (it.get("end_time").toString());
-                    end_time2 = end_time2.substring(0, 5);
-                    timetem += s_day + ' ' + start_time2 + '-' + end_time2;
-                    ansit.put("course_time", timetem);
-                    break;
-                }
-            }
-            if (newone) {
-                Object day = it.remove("day");
-                Object start_time = it.remove("start_time");
-                Object end_time = it.remove("end_time");
-                Object building = it.remove("building");
-                Object room_number = it.remove("room_number");
-                int i_day = (int) day;
-                String s_day = null;
-                switch (i_day) {
-                    case 1:
-                        s_day = "周一";
-                        break;
-                    case 2:
-                        s_day = "周二";
-                        break;
-                    case 3:
-                        s_day = "周三";
-                        break;
-                    case 4:
-                        s_day = "周四";
-                        break;
-                    case 5:
-                        s_day = "周五";
-                        break;
-                    case 6:
-                        s_day = "周六";
-                        break;
-                    case 7:
-                        s_day = "周日";
-                        break;
-                }
+    }
 
-                String start_time2 = (start_time.toString());
-                start_time2 = start_time2.substring(0, 5);
-                String end_time2 = (end_time.toString());
-                end_time2 = end_time2.substring(0, 5);
-                it.put("course_place", (String) building + ' ' + (String) room_number);
-                it.put("course_time", s_day + ' ' + start_time2 + '-' + end_time2);
-                zhuList.add(it);
-            }
-
+    public List<Map<String, Object>> getBeiList(int limit, int page, HttpSession session, String searchParams) throws IOException {
+        User user = (User) session.getAttribute("user");
+        Student student = (Student) user.getPerson();
+        JSONObject jsonObject;
+        if (searchParams != null) {
+            jsonObject = JSONObject.parseObject(searchParams);
+        } else {
+            jsonObject = new JSONObject();
         }
+        return hasSelectedCoursesService.getHasSelectedCourses(student.getStudentId(), limit, page, jsonObject.getString("class_name"), jsonObject.getInteger("year"), jsonObject.getString("semester"),0);
+    }
+
+    @RequestMapping(value = "/student/selected_major_courses.json", produces = "application/json;charset=UTF-8")
+    public JSONObject getSelectedMajorCourse(int limit, int page, HttpSession session, String searchParams) throws IOException {
+
+        List<Map<String, Object>> zhuList = getZhuList(limit,page,session,searchParams);
 
         JsonUtil jsonUtil = new JsonUtil(200, "");
         jsonUtil.put("count", zhuList.size());
@@ -147,108 +64,8 @@ public class HasSelectedCoursesController {
 
     @RequestMapping(value = "/student/selected_alternative_courses.json", produces = "application/json;charset=UTF-8")
     public JSONObject getSelectedAlternativeCourse(int limit, int page, HttpSession session, String searchParams) throws IOException {
-        User user = (User) session.getAttribute("user");
-        Student student = (Student) user.getPerson();
-        JSONObject jsonObject;
-        if (searchParams != null) {
-            jsonObject = JSONObject.parseObject(searchParams);
-        } else {
-            jsonObject = new JSONObject();
-        }
-        List<Map<String, Object>> selectCoursesList =new ArrayList<>();
-        selectCoursesList = hasSelectedCoursesService.getHasSelectedCourses(student.getStudentId(), limit, page, jsonObject.getString("class_name"), jsonObject.getInteger("year"), jsonObject.getString("semester"),0);
 
-        //将相同class_id的合并
-        for (Map<String, Object> it : selectCoursesList) {
-            boolean newone = true;
-            //找在不在ansList里
-            for (Map<String, Object> ansit : beiList) {
-                if (ansit.get("class_id") == it.get("class_id")) {
-                    newone = false;
-                    String tem = (String) ansit.get("course_place");
-                    tem += "<br>";
-                    tem += (String) it.get("building") + " " + (String) it.get("room_number");
-                    ansit.put("course_place", tem);
-                    String timetem = (String) ansit.get("course_time");
-                    timetem += "<br>";
-                    int i_day = (int) it.get("day");
-                    String s_day = null;
-                    switch (i_day) {
-                        case 1:
-                            s_day = "周一";
-                            break;
-                        case 2:
-                            s_day = "周二";
-                            break;
-                        case 3:
-                            s_day = "周三";
-                            break;
-                        case 4:
-                            s_day = "周四";
-                            break;
-                        case 5:
-                            s_day = "周五";
-                            break;
-                        case 6:
-                            s_day = "周六";
-                            break;
-                        case 7:
-                            s_day = "周日";
-                            break;
-                    }
-
-                    String start_time2 = (it.get("start_time").toString());
-                    start_time2 = start_time2.substring(0, 5);
-                    String end_time2 = (it.get("end_time").toString());
-                    end_time2 = end_time2.substring(0, 5);
-                    timetem += s_day + ' ' + start_time2 + '-' + end_time2;
-                    ansit.put("course_time", timetem);
-                    break;
-                }
-            }
-            if (newone) {
-                Object day = it.remove("day");
-                Object start_time = it.remove("start_time");
-                Object end_time = it.remove("end_time");
-                Object building = it.remove("building");
-                Object room_number = it.remove("room_number");
-                int i_day = (int) day;
-                String s_day = null;
-                switch (i_day) {
-                    case 1:
-                        s_day = "周一";
-                        break;
-                    case 2:
-                        s_day = "周二";
-                        break;
-                    case 3:
-                        s_day = "周三";
-                        break;
-                    case 4:
-                        s_day = "周四";
-                        break;
-                    case 5:
-                        s_day = "周五";
-                        break;
-                    case 6:
-                        s_day = "周六";
-                        break;
-                    case 7:
-                        s_day = "周日";
-                        break;
-                }
-
-                String start_time2 = (start_time.toString());
-                start_time2 = start_time2.substring(0, 5);
-                String end_time2 = (end_time.toString());
-                end_time2 = end_time2.substring(0, 5);
-                it.put("course_place", (String) building + ' ' + (String) room_number);
-                it.put("course_time", s_day + ' ' + start_time2 + '-' + end_time2);
-                beiList.add(it);
-            }
-
-        }
-
+        List<Map<String, Object>> beiList =  getBeiList(limit,page,session,searchParams);
         JsonUtil jsonUtil = new JsonUtil(200, "");
         jsonUtil.put("count", beiList.size());
         jsonUtil.put("data", beiList);
--- ClassRegistrationSystem/src/main/java/cn/edu/jlu/ccst/glzz/system/Service/HasSelectedCoursesService.java
+++ ClassRegistrationSystem/src/main/java/cn/edu/jlu/ccst/glzz/system/Service/HasSelectedCoursesService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -34,8 +35,98 @@ public class HasSelectedCoursesService {
             query.eq("semester",semester);
         }
         List<String> column = Arrays.asList("class_id","course_id","professor_name","title as class_name","credits","course.dept_name","sec_capacity","building","room_number","day","start_time","end_time","ismajor");
-        List<Map<String,Object>> selectCourses=takesDao.listMap(column,query);
+        List<Map<String,Object>> selectCoursesList=takesDao.listMap(column,query);
+        //将相同class_id的合并
+        List<Map<String, Object>> zhuList = new ArrayList<>();
+        for (Map<String, Object> it : selectCoursesList) {
+            boolean newone = true;
+            //找在不在ansList里
+            for (Map<String, Object> ansit : zhuList) {
+                if (ansit.get("class_id") == it.get("class_id")) {
+                    newone = false;
+                    String tem = (String) ansit.get("course_place");
+                    tem += "<br>";
+                    tem += (String) it.get("building") + " " + (String) it.get("room_number");
+                    ansit.put("course_place", tem);
+                    String timetem = (String) ansit.get("course_time");
+                    timetem += "<br>";
+                    int i_day = (int) it.get("day");
+                    String s_day = null;
+                    switch (i_day) {
+                        case 1:
+                            s_day = "周一";
+                            break;
+                        case 2:
+                            s_day = "周二";
+                            break;
+                        case 3:
+                            s_day = "周三";
+                            break;
+                        case 4:
+                            s_day = "周四";
+                            break;
+                        case 5:
+                            s_day = "周五";
+                            break;
+                        case 6:
+                            s_day = "周六";
+                            break;
+                        case 7:
+                            s_day = "周日";
+                            break;
+                    }
 
-        return selectCourses;
+                    String start_time2 = (it.get("start_time").toString());
+                    start_time2 = start_time2.substring(0, 5);
+                    String end_time2 = (it.get("end_time").toString());
+                    end_time2 = end_time2.substring(0, 5);
+                    timetem += s_day + ' ' + start_time2 + '-' + end_time2;
+                    ansit.put("course_time", timetem);
+                    break;
+                }
+            }
+            if (newone) {
+                Object day = it.remove("day");
+                Object start_time = it.remove("start_time");
+                Object end_time = it.remove("end_time");
+                Object building = it.remove("building");
+                Object room_number = it.remove("room_number");
+                int i_day = (int) day;
+                String s_day = null;
+                switch (i_day) {
+                    case 1:
+                        s_day = "周一";
+                        break;
+                    case 2:
+                        s_day = "周二";
+                        break;
+                    case 3:
+                        s_day = "周三";
+                        break;
+                    case 4:
+                        s_day = "周四";
+                        break;
+                    case 5:
+                        s_day = "周五";
+                        break;
+                    case 6:
+                        s_day = "周六";
+                        break;
+                    case 7:
+                        s_day = "周日";
+                        break;
+                }
+
+                String start_time2 = (start_time.toString());
+                start_time2 = start_time2.substring(0, 5);
+                String end_time2 = (end_time.toString());
+                end_time2 = end_time2.substring(0, 5);
+                it.put("course_place", (String) building + ' ' + (String) room_number);
+                it.put("course_time", s_day + ' ' + start_time2 + '-' + end_time2);
+                zhuList.add(it);
+            }
+
+        }
+        return zhuList;
     }
 }
