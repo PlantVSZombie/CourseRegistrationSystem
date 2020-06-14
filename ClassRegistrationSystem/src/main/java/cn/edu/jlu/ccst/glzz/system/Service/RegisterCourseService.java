fc19b8a49a6c0c1af2e5a382d7392dffa9ec1f0e
chelseazqc <39674376+chelseazqc@users.noreply.github.com>
2020-06-14 20:29:34 +0800

    完成了学生信息维护与教授信息维护，以及分页bug的修复

--- ClassRegistrationSystem/src/main/java/cn/edu/jlu/ccst/glzz/system/Service/RegisterCourseService.java
+++ ClassRegistrationSystem/src/main/java/cn/edu/jlu/ccst/glzz/system/Service/RegisterCourseService.java
@@ -1,6 +1,7 @@
 package cn.edu.jlu.ccst.glzz.system.Service;
 
 import cn.edu.jlu.ccst.glzz.system.generated.DAO.FlowControlDao;
+import cn.edu.jlu.ccst.glzz.system.generated.DAO.ProfessorDao;
 import cn.edu.jlu.ccst.glzz.system.generated.DAO.StudentDao;
 import org.apache.ibatis.annotations.Update;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,8 @@ public class RegisterCourseService {
     FlowControlDao flowControlDao;
     @Resource
     StudentDao studentDao;
+    @Resource
+    ProfessorDao professorDao;
 
 
 
@@ -35,12 +38,91 @@ public class RegisterCourseService {
         return control_flow;
 
     }
-    public List<Map<String,Object>> getStudentInfo(int limit,int page){
+    public Query getStudentInfoQuery(String student_id,String student_name,String dept_name){
         Query query=new Query();
-        query.page(page,limit);
 
+        if(student_id!=null&&!student_id.equals("")){
+            query.eq("student_id",student_id);
+        }
+        if(student_name!=null&&!student_name.equals("")){
+            query.eq("student_name",student_name);
+        }
+        if(dept_name!=null&&!dept_name.equals("")){
+            query.eq("dept_name",dept_name);
+        }
+
+
+        return query;
+    }
+    public List<Map<String,Object>> getStudentInfo(String student_id,String student_name,String dept_name,int limit,int page){
+        Query query=getStudentInfoQuery(student_id,student_name,dept_name);
+        query.page(page,limit);
         List<String> column = Arrays.asList("student_id","student_name","dept_name");
-        List<Map<String,Object>> studentInfo=studentDao.listMap(column,query);
-        return studentInfo;
+        List<Map<String,Object>> studentinfo=studentDao.listMap(column,query);
+        return studentinfo;
+    }
+    public long getStudentInfoCount(String student_id,String student_name,String dept_name){
+        return studentDao.getCount(getStudentInfoQuery(student_id,student_name,dept_name));
+    }
+
+
+
+
+
+    public Query getProfessorInfoQuery(String professor_id,String professor_name,String dept_name){
+        Query query=new Query();
+
+        if(professor_id!=null&&!professor_id.equals("")){
+            query.eq("professor_id",professor_id);
+        }
+        if(professor_name!=null&&!professor_name.equals("")){
+            query.eq("professor_name",professor_name);
+        }
+        if(dept_name!=null&&!dept_name.equals("")){
+            query.eq("dept_name",dept_name);
+        }
+        return query;
+    }
+    public List<Map<String,Object>> getProfessorInfo(String professor_id,String professor_name,String dept_name,int limit,int page){
+        Query query=getProfessorInfoQuery(professor_id,professor_name,dept_name);
+        query.page(page,limit);
+        List<String> column = Arrays.asList("professor_id","professor_name","dept_name");
+        List<Map<String,Object>> professorinfo=professorDao.listMap(column,query);
+        return professorinfo;
+    }
+
+    public long getProfessorInfoCount(String professor_id,String professor_name,String dept_name){
+        return professorDao.getCount(getProfessorInfoQuery(professor_id,professor_name,dept_name));
+    }
+
+
+
+    public void addStudent(String student_name,String dept_name,String password){
+        int id=studentDao.getMax()+1;
+        studentDao.insert(id+"",password,student_name,dept_name,0);
+    }
+    public void addProfessor(String professor_name,String dept_name,String password){
+        int id=professorDao.getMax()+1;
+        professorDao.insert(id+"",password,professor_name,dept_name);
+    }
+
+
+    public void editStudent(String student_id,String student_name,String dept_name,String password){
+
+        studentDao.updateById(student_id,student_name,dept_name,password);
+
+    }
+    public void editProfessor(String professor_id,String professor_name,String dept_name,String password){
+        System.out.println("edit_id"+professor_id);
+        professorDao.updateById(professor_id,professor_name,dept_name,password);
+
+    }
+
+
+    public void deleteStudent(String student_id){
+        studentDao.deleteStudent(student_id);
+    }
+    public void deleteProfessor(String professor_id){
+        professorDao.deleteProfessor(professor_id);
     }
 }
--- ClassRegistrationSystem/src/main/java/cn/edu/jlu/ccst/glzz/system/generated/DAO/StudentDao.java
+++ ClassRegistrationSystem/src/main/java/cn/edu/jlu/ccst/glzz/system/generated/DAO/StudentDao.java
@@ -3,10 +3,22 @@ package cn.edu.jlu.ccst.glzz.system.generated.DAO;
 import cn.edu.jlu.ccst.glzz.system.generated.Model.Student;
 
 import com.gitee.fastmybatis.core.mapper.CrudMapper;
+import org.apache.ibatis.annotations.*;
 
 
 /**
  * @author glzz
  */
 public interface StudentDao extends CrudMapper<Student, String> {
+    @Insert("insert into student (student_id,password,student_name,dept_name,tot_cred)values(#{student_id},#{password},#{student_name},#{dept_name},#{tot_cred})")
+    int insert(@Param("student_id") String student_id,@Param("password") String password, @Param("student_name") String student_name, @Param("dept_name") String dept_name, @Param("tot_cred") int tot_cred);
+    @Select("select max(student_id) from student")
+    int getMax();
+    @Select("select student_id from student where password = #{password},student_name = #{student_name},dept_name = #{dept_name}")
+    int searchAndReturnID(@Param("password") String password,@Param("student_name") String student_name,@Param("dept_name") String dept_name);
+    @Delete("delete from student where student_id = #{student_id}")
+    int deleteStudent(@Param("student_id") String student_id);
+    @Update("update student set student_name = #{student_name},dept_name = #{dept_name},password = #{password} where student_id = #{student_id}")
+    int updateById(@Param("student_id") String student_id, @Param("student_name") String student_name, @Param("dept_name") String dept_name, @Param("password") String password);
+
 }
