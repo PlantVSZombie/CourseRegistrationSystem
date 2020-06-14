3c2b08f575d92f6549978a1954cba355bf04bf2a
T-will19 <1329515363@qq.com>
2020-06-14 15:28:27 +0800

    提交学生成绩Test

--- ClassRegistrationSystem/src/main/java/cn/edu/jlu/ccst/glzz/system/Controller/ShowStudentTableController.java
+++ ClassRegistrationSystem/src/main/java/cn/edu/jlu/ccst/glzz/system/Controller/ShowStudentTableController.java
@@ -35,7 +35,7 @@ public class ShowStudentTableController {
 
         }
         List<Map<String,Object>> student_table=showStudentTableService.getStudentTable(professor.getProfessorId(),limit, page,
-                jsonObject.getString("course_id"),jsonObject.getString("title"),jsonObject.getInteger("class_id"),
+                jsonObject.getString("course_id"),jsonObject.getString("title"),jsonObject.getInteger("sec_id"),
                 jsonObject.getString("semester"),jsonObject.getInteger("year"),jsonObject.getDouble("credits"),
                 jsonObject.getString("student_name"));
         JsonUtil jsonUtil=new JsonUtil(200,"");
@@ -48,10 +48,10 @@ public class ShowStudentTableController {
 
 
     @RequestMapping(value="/professor/correctGrade.json",produces="application/json;charset=UTF-8")
-    public Result correctGrade(HttpSession session,int class_id,String student_id,String grade)throws IOException{
+    public Result correctGrade(HttpSession session,int sec_id,String student_id,String grade)throws IOException{
         User user=(User)session.getAttribute("user");
 
-        showStudentTableService.correctGrade(class_id,student_id,grade);
+        showStudentTableService.correctGrade(sec_id,student_id,grade);
 
 
         return Result.ok();
--- ClassRegistrationSystem/src/main/java/cn/edu/jlu/ccst/glzz/system/Service/ShowStudentTableService.java
+++ ClassRegistrationSystem/src/main/java/cn/edu/jlu/ccst/glzz/system/Service/ShowStudentTableService.java
@@ -20,7 +20,7 @@ public class ShowStudentTableService {
     TakesDao takesDao;
 
     public List<Map<String,Object>> getStudentTable(String professor_id, int limit, int page, String course_id,
-                                                    String title,Integer class_id,String semester,Integer year,
+                                                    String title,Integer sec_id,String semester,Integer year,
                                                     Double credits,String student_name){
         Query query_1=new Query();
         query_1.join("natural join section join takes join student join course ");
@@ -41,8 +41,8 @@ public class ShowStudentTableService {
         if(course_id!=null&&!course_id.equals("")){
             query_1.eq("course.course_id",course_id);
         }
-        if(class_id!=null){
-            query_1.eq("takes.class_id",class_id);
+        if(sec_id!=null){
+            query_1.eq("sec_id",sec_id);
         }
         if(credits!=null){
             query_1.eq("credits",credits);
@@ -52,26 +52,28 @@ public class ShowStudentTableService {
         }
 
         if(professor_id!=null&&((title!=null&&!title.equals(""))||(year!=null)||(semester!=null&&!semester.equals(""))||
-                (course_id!=null&&!course_id.equals(""))||(class_id!=null)||(credits!=null)||
+                (course_id!=null&&!course_id.equals(""))||(sec_id!=null)||(credits!=null)||
                 (student_name!=null&&!student_name.equals("")))){
-            List<String> column = Arrays.asList("course.course_id","title","takes.class_id","semester","year","credits","student.student_id",
+            List<String> column = Arrays.asList("course.course_id","title","sec_id","semester","year","credits","student.student_id",
                     "student_name","grade");
             List<Map<String,Object>> course_table=teachesDao.listMap(column,query_1);
             return course_table;
         }
         else {
-            List<String> column = Arrays.asList("distinct course.course_id", "title", "takes.class_id", "semester", "year", "credits");
+            List<String> column = Arrays.asList("distinct course.course_id", "title", "sec_id", "semester", "year", "credits");
             List<Map<String, Object>> course_table = teachesDao.listMap(column, query_1);
             return course_table;
         }
     }
 
-    public void correctGrade(Integer class_id,String student_id,String grade){
+    public void correctGrade(Integer sec_id,String student_id,String grade){
         Query query = new Query();
-        query.eq("class_id",class_id);
+        query.join(" natural join section ");
+        query.eq("sec_id",sec_id);
         query.eq("student_id",student_id);
         Takes takes = takesDao.getByQuery(query);
         takes.setGrade(grade);
+        takes.setStatus("scored");
         takesDao.updateByQuery(takes,query);
     }
 
