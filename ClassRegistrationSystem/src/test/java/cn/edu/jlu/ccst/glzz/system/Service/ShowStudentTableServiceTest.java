820112e097c7aa8ca8a1ce37fd90b4377bedd2ed
T-will19 <1329515363@qq.com>
2020-06-14 16:12:50 +0800

    提交学生成绩Test

---
+++ ClassRegistrationSystem/src/test/java/cn/edu/jlu/ccst/glzz/system/Service/ShowStudentTableServiceTest.java
@@ -0,0 +1,16 @@
+package cn.edu.jlu.ccst.glzz.system.Service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShowStudentTableServiceTest {
+
+    @Test
+    void getStudentTable() {
+    }
+
+    @Test
+    void correctGrade() {
+    }
+}
